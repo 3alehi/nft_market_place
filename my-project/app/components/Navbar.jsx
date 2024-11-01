817d6117cfe56d735a9dd9999818ebd0166c0418
3alehi <3alehi.ah.com>
2024-11-01 08:26:02 -0700

    hamberger menu

--- my-project/app/components/Navbar.jsx
+++ my-project/app/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className='mx-10 my-12 flex justify-between items-center'>
+    <div className='md:mx-10 md:my-12 max-md:my-6 max-md:mx-3 flex justify-between items-center'>
 
 <Link href={"#"}>
 <div className='flex items-center cursor-pointer  '>
@@ -18,7 +18,7 @@ export default function Navbar() {
 
 </div>
 </Link>
-<ul className='flex items-center'>
+<ul className='flex items-center max-md:hidden'>
     <Link className='mx-3' href={"#"}>Marketplace</Link>
     <Link className='mx-3' href={"#"}>Rankings</Link>
     <Link className='mx-3' href={"#"}>Connect a Wallet</Link>
@@ -34,6 +34,15 @@ export default function Navbar() {
     
 
 </ul>
+{/* amberget Menu */}
+<div className='hidden max-md:block'>
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fillRule="evenodd" clip-rule="evenodd" d="M3 12C3 11.5858 3.33579 11.25 3.75 11.25H20.25C20.6642 11.25 21 11.5858 21 12C21 12.4142 20.6642 12.75 20.25 12.75H3.75C3.33579 12.75 3 12.4142 3 12Z" fill="white"/>
+<path fillRule="evenodd" clip-rule="evenodd" d="M3 6C3 5.58579 3.33579 5.25 3.75 5.25H15C15.4142 5.25 15.75 5.58579 15.75 6C15.75 6.41421 15.4142 6.75 15 6.75H3.75C3.33579 6.75 3 6.41421 3 6Z" fill="white"/>
+<path fillRule="evenodd" clip-rule="evenodd" d="M3 18C3 17.5858 3.33579 17.25 3.75 17.25H15C15.4142 17.25 15.75 17.5858 15.75 18C15.75 18.4142 15.4142 18.75 15 18.75H3.75C3.33579 18.75 3 18.4142 3 18Z" fill="white"/>
+</svg>
+
+</div>
 
 
     </div>
