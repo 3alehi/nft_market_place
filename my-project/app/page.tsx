d73d7f6ca098dc90be578e28d72b8f351010e182
3alehi <3alehi.ah.com>
2024-11-01 08:48:38 -0700

    items in header

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import Header from "./components/Header"
 export default function page() {
   return (
-    <div>page</div>
+    <div>
+      <Header/>
+    </div>
   )
 }
